3e85ebe3d60d26d10159c2407b44efb1782a1e19
Alexandre <alebencz@gmail.com>
2013-06-13 10:14:00 -0300

    Signed-off-by: Alexandre <alebencz@gmail.com>

--- compiler.c
+++ compiler.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#define lower(x) (((x)>0x40 && (x)<0x5b)?(x)|0x20:(x))
+
+#define READBUFFERSIZE	512
+#define WRITEBUFFERSIZE	512
+
+// table to store labels, names and values
+typedef struct sltable
+{
+	struct sltable *next;
+	char *name;
+	unsigned long value;
+} tltable;
+
+// table to store equate names and values
+typedef struct setable
+{
+	struct setable *next;
+	char *name;
+	char *value;
+	char flag;
+} tetable;
+
+tltable *ltable = NULL;
+tetable *etable = NULL;
+
+typedef struct
+{
+	char name[4];
+	unsigned char value;
+	unsigned long flag;
+} tregisters;
+
+/* #define NUMREGS 20
+tregisters registers[] =
+{
+}; */
+
+char outFile[13], ext[]=".com";
 int main(int argc, char **argv)
 {
 	int i;
@@ -8,6 +47,36 @@ int main(int argc, char **argv)
 
 	inFile = argv[1];
 	outFileStart = inFile;
+
+	while(*inFile)
+	{
+		if(*inFile == "\\")
+			outFileStart = inFile+1;
+		inFile++;
+	}
+
+	inFile = outFile;
+
+	while(*outFileStart != '.' && *outFileStart)
+	{
+		*inFile = *outFileStart;
+		inFile++;
+		outFileStart++;
+	}
+	*inFile = 0;
+
+	if(argc <= 1)
+		printf("Modo de uso: asmBuilder <fileName>\n");
+	else
+	{
+		fOutPut = fopen(outFile, "wb");
+
+		for(i=1;i<3;i++)
+		{
+		}
+
+		fclose(fOutPut);
+	}
 
 	return 0;
 }
