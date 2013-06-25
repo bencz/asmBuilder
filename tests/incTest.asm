a2acbd82b830235756c38df31290a6daa3e3bc70
Alexandre <alebencz@gmail.com>
2013-06-25 00:13:53 -0300

    Signed-off-by: Alexandre <alebencz@gmail.com>

---
+++ tests/incTest.asm
@@ -0,0 +1,27 @@
+;--------------------------------------------------------------------------- 
+; Hello World program
+; written by Alexandre Savelli Bencz
+;--------------------------------------------------------------------------- 
+
+.include "std.inc"
+
+;--------------------------------------------------------------------------- 
+; here comes the code  
+;--------------------------------------------------------------------------- 
+
+  call @hello                 ; call hello world procedure
+  jmp @exit
+
+@hello:
+  push cs                     ; put cs onto the stack
+  pop  ds                     ; retrieve cs into ds
+  mov  dx,offset HelloWorld   ; move string offset into dx
+  mov  ah,9                   ; ah<-9 DOS print string
+  int  021h                   ; call DOS interrupt
+  ret                         ; return back to caller
+
+;---------------------------------------------------------------------------
+;  now here's the data
+;---------------------------------------------------------------------------
+
+HelloWorld  db "Hello World!$"
